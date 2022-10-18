ee00f996335a71e3b5df837950b089bc01682790
alibboye2000 <alibboye84@gmail.com>
2022-10-18 18:16:55 +0700

    first apache

---
+++ src/test/java/alibatis/demo/FirsttaskApplicationTests.java
@@ -0,0 +1,13 @@
+package alibatis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirsttaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
